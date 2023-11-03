175640528a44f14213076c7fd95253d0465fdec2
RoleyRoley <119747877+RoleyRoley@users.noreply.github.com>
2023-11-03 17:26:33 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log('Hellow from JavaScript');
-<script src="script.js"></script>
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+
+    const isDarkmode = body.classList.contains('dark-mode');
+    darkModeToggle.textContent = isDarkmode ? 'Toggle Light Mode' : 'Toggle Dark Mode';
+});
+
+
+
